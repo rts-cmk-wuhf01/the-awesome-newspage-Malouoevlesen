1ad4d9d3576bf106651fbf575fa28db2297c69c1
Malou Øvlesen <malouoevlesen@gmail.com>
2019-06-04 10:50:10 +0200

    Started over once more. Gone back to previous commit.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,19 +4,4 @@ module.exports = (app) => {
       res.render('home');
    });
 
-   app.get('/about', (req, res, next) => {
-      res.render('about');
-   });
-
-   app.get('/contact', (req, res, next) => {
-      res.render('contact');
-   });
-
-   app.get('/categories-post', (req, res, next) => {
-      res.render('categories-post');
-   });
-
-   app.get('/single-post', (req, res, next) => {
-      res.render('single-post');
-   });
 };
