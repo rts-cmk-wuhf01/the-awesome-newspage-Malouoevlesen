02772f413299a12546f466fa866abf34509d1408
Malou Øvlesen <malouoevlesen@gmail.com>
2019-06-19 12:26:47 +0200

    Added alot more comments.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,18 +2,32 @@ const mySQL = require("../config/mysql");
 
 module.exports = (app) => {
 
+   // Route - Process of showing a packet of data from one source to a destination. 
+
+   // Require, request. 
    app.get('/', async (req, res, next) => {
       // Connect to the DB and get all the data
+      // The await operator is used to wait for a Promise. It can only be used inside an async function.
       db = await mySQL.connect (); // Connects to database.      
       let [categories] = await db.query('SELECT * FROM categories');
       db.end (); // Disconnects from database.
 
+      // Call back function purely for the purpose of testing. 
       res.render('home', { categories:categories }, function (err, html) {
          res.send(html)
+
+      // Rendering is the process of gathering data and load the associated templates (or send the output directly). Then apply the gathered data to the associated templates. The final output is sent to the user.
+
+      // Also determines which ejs file it uses. 
+      
+
       })
 
    });
    
+   app.get('/hidden', function (req, res) {
+      res.send('hello world')
+    })
 
    app.get('/about', async (req, res, next) => {
          // Connect to the DB and get all the data
@@ -36,11 +50,12 @@ module.exports = (app) => {
             res.send(html)
          })
 
-         //  tilføjes i routes.js filen f.eks. lige under app.get('/contact') endpoint
-         app.post('/contact', (req, res, next) => {
-         res.send(req.body);
-         });
    });
+
+            //  tilføjes i routes.js filen f.eks. lige under app.get('/contact') endpoint
+            app.post('/contact', (req, res, next) => {
+               res.send(req.body);
+               });
 
 
 
@@ -63,9 +78,9 @@ module.exports = (app) => {
       let [categories] = await db.query('SELECT * FROM categories');
       let [articles] = await db.execute(`
          SELECT articles.title, articles.text, images.src AS img, articles.id, 
-         (SELECT COUNT(comments.id) 
-         FROM comments 
-         WHERE article_fk = articles.id) AS article_comments
+            (SELECT COUNT(comments.id) 
+            FROM comments 
+            WHERE article_fk = articles.id) AS article_comments
          FROM articles 
          INNER JOIN images ON articles.images_fk=images.id 
          WHERE articles.category_fk = ?
@@ -74,6 +89,8 @@ module.exports = (app) => {
       console.log(articles)
 
       db.end (); // Disconnects from database.
+
+      
 
       console.log(categories)
 
@@ -85,6 +102,59 @@ module.exports = (app) => {
 
    });
 
+
+
+   app.get('/categories-post2/:category_id', async (req, res, next) => {
+
+      let SQL = "";
+      let SQLvalues = [];
+
+      // Connect to the DB and get all the data
+      db = await mySQL.connect (); // Connects to database. 
+      SQL = 'SELECT * FROM categories';
+
+      let [categories] = await db.query(SQL);
+
+       // ========
+      SQL = `
+      SELECT articles.title, articles.text, images.src AS img, articles.id, 
+         (SELECT COUNT(comments.id) 
+         FROM comments 
+         WHERE article_fk = articles.id) AS article_comments
+      FROM articles 
+      INNER JOIN images ON articles.images_fk=images.id 
+      WHERE articles.category_fk = ?
+
+   `;
+
+
+   SQLvalues = [req.params.category_id];
+
+      let [articles] = await db.execute (SQL, SQLvalues);
+
+      // ========
+
+     /* let [articles] = await db.execute(`
+         SELECT articles.title, articles.text, images.src AS img, articles.id, 
+            (SELECT COUNT(comments.id) 
+            FROM comments 
+            WHERE article_fk = articles.id) AS article_comments
+         FROM articles 
+         INNER JOIN images ON articles.images_fk=images.id 
+         WHERE articles.category_fk = ?
+
+      `, [req.params.category_id]);
+      console.log(articles)
+*/
+      db.end (); // Disconnects from database.
+
+     
+      let dateForPost = new Date('2019-04-14 07:00:14');
+      let dateForOtherPost = new Date('2019-04-14 11:00:14');
+      res.render('categories-post', { date: dateForPost, dateOther: dateForOtherPost, categories: categories, articles: articles}, function (err, html) {
+         res.send(html)
+      })
+
    // Kept for notes. 
    // app.get('/categories-post', async (req, res, next) => {
    //    let dateForPost = new Date('2019-04-14 07:00:14');
@@ -93,6 +163,6 @@ module.exports = (app) => {
    //       res.send(html)
    //    })
 
-   // });
+   });
 
 };
